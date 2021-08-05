37f20a90c178fd43d3dbddbc5d4bc7007ece1d24
jr0dd <j_r0dd@icloud.com>
2021-08-05 15:03:48 -0400

    .

--- hack/gen-chart-summary.sh
+++ hack/gen-chart-summary.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+
 # require yq
 command -v yq >/dev/null 2>&1 || {
     echo >&2 "yq (https://github.com/mikefarah/yq) is not installed. Aborting."
@@ -11,17 +13,17 @@ charts_folder="${repository}/charts"
 charts_summary_file="${charts_folder}"/README.md
 
 # Gather all charts using the common library, excluding common-test
-stable_charts=$(find "${charts_folder}" -name "Chart.yaml" | sort)
+charts=$(find "${charts_folder}" -name "Chart.yaml" | sort)
 
 echo "# Helm charts overview" > "${charts_summary_file}"
 
 echo "### Stable charts:" >> "${charts_summary_file}"
 echo "| Chart | Description |" >> "${charts_summary_file}"
 echo "| ----- | ----------- |" >> "${charts_summary_file}"
-for i in ${stable_charts[@]}
+for i in ${charts[@]}
 do
     chart_data=($(yq eval '.name, .description' "$i"))
     chart_name="${chart_data[0]}"
     chart_description="${chart_data[@]:1}"
-    echo "| [${chart_name}](${chart_name}) | ${chart_description} |" >> "${charts_summary_file}"
+    echo "| [${chart_name}] | ${chart_description} |" >> "${charts_summary_file}"
 done
