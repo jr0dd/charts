d7226d9d11c700da2c030edc5fa9ae712569ae16
jr0dd <j_r0dd@icloud.com>
2021-08-06 08:25:35 -0400

    moar automation tweaks

--- hack/gen-helmdocsignore.sh
+++ hack/gen-helmdocsignore.sh
@@ -8,7 +8,7 @@ set -eu
 # shellcheck disable=SC2155
 export REPO_ROOT=$(git rev-parse --show-toplevel)
 
-grep -L -r "\- name\: common" ./charts/*/Chart.yaml \
+grep -L -r "\- name\: common" ./charts/**/Chart.yaml \
     | sed 's|\.\/\(.*\)/Chart.yaml|\1|' \
     | grep -v "common" \
     > .helmdocsignore
