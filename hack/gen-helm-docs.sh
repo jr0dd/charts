9586b9b7a73661fdb58c43e9ac45268d29fccbcf
jr0dd <j_r0dd@icloud.com>
2021-08-08 13:11:43 -0400

    tweak helm-docs for my repo

--- hack/gen-helm-docs.sh
+++ hack/gen-helm-docs.sh
@@ -2,8 +2,7 @@
 set -eu
 
 # Generate helm-docs for Helm charts
-# Usage ./gen-helm-docs.sh [chart]
-
+                                                                                                                                                                                                                            
 # require helm-docs
 command -v helm-docs >/dev/null 2>&1 || {
     echo >&2 "helm-docs (https://github.com/norwoodj/helm-docs) is not installed. Aborting."
@@ -14,29 +13,30 @@ command -v helm-docs >/dev/null 2>&1 || {
 repository=$(git rev-parse --show-toplevel)
 
 # Templates to copy into each chart directory
-readme_template="${repository}/hack/templates/README.md.gotmpl"
-readme_config_template="${repository}/hack/templates/README_CONFIG.md.gotmpl"
-readme_changelog_template="${repository}/hack/templates/README_CHANGELOG.md.gotmpl"
+templates="${repository}/hack/templates"
+readme="${templates}/README.md.gotmpl"
+readme_config="${templates}/README_CONFIG.md.gotmpl"
+readme_changelog="${templates}/README_CHANGELOG.md.gotmpl"
 
 # Gather all charts using the common library, excluding common-test
 charts=$(find "${repository}" -name "Chart.yaml")
 root="${repository}/charts"
 
 for chart in ${charts}; do
-    chart_directory="$(dirname "${chart}")"
-    echo "-] Copying templates to ${chart_directory}"
+    chart_dir="$(dirname "${chart}")"
+    echo "-] Copying templates to ${chart_dir}"
     # Copy README template into each Chart directory, overwrite if exists
-    cp "${readme_template}" "${chart_directory}"
+    cp "${readme}" "${chart_dir}"
     # Copy CONFIG template to each Chart directory, do not overwrite if exists
-    cp -n "${readme_config_template}" "${chart_directory}" || true
+    cp -n "${readme_config}" "${chart_dir}" || true
     # Copy CHANGELOG template to each Chart directory, do not overwrite if exists
-    cp -n "${readme_changelog_template}" "${chart_directory}" || true
+    cp -n "${readme_changelog}" "${chart_dir}" || true
 done
 
 # Run helm-docs for charts using the common library and the common library itself
 helm-docs \
     --ignore-file="${repository}/.helmdocsignore" \
-    --template-files="$(basename "${readme_template}")" \
-    --template-files="$(basename "${readme_config_template}")" \
-    --template-files="$(basename "${readme_changelog_template}")" \
+    --template-files="${readme}" \
+    --template-files="${readme_config}" \
+    --template-files="${readme_changelog}" \
     --chart-search-root="${root}"
