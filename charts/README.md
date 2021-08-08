caf43ea27522e89091b3d3129150593661bc5302
jr0dd <j_r0dd@icloud.com>
2021-08-07 22:49:36 -0400

    force index recreation

--- charts/README.md
+++ charts/README.md
@@ -1,5 +1,7 @@
 # Helm charts overview
+
 ## Charts:
+
 | Chart | Description |
 | ----- | ----------- |
 | [cloudflare-ddns](cloudflare-ddns) | cloudflare ddns updater |
--- hack/gen-chart-summary.sh
+++ hack/gen-chart-summary.sh
@@ -15,9 +15,9 @@ charts_summary_file="${charts_folder}"/README.md
 # Gather all charts using the common library, excluding common-test
 stable_charts=$(find "${charts_folder}" -name "Chart.yaml" | sort)
 
-echo "# Helm charts overview" > "${charts_summary_file}"
+echo $'# Helm charts overview\n' > "${charts_summary_file}"
 
-echo "## Charts:" >> "${charts_summary_file}"
+echo $'## Charts:\n' >> "${charts_summary_file}"
 echo "| Chart | Description |" >> "${charts_summary_file}"
 echo "| ----- | ----------- |" >> "${charts_summary_file}"
 for i in ${stable_charts[@]}
